e4a8e0070fe9536c8bc4e2d52969b208bd2fe0b9
Daniel Sumsion <dsumsio@byu.edu>
2024-09-10 06:14:10 -0600

    adding data and EDA

--- Bike.R
+++ Bike.R
@@ -1,2 +1,36 @@
 library(tidyverse)
 library(tidymodels)
+library(vroom)
+library(GGally)
+library(corrplot)
+library(patchwork)
+
+data <- vroom("train.csv")
+
+
+corr <- corrplot(cor(data[sapply(data, is.numeric)]), type = "upper")
+
+humid <- ggplot(data, mapping=aes(x=humidity, y = count)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+temp <- ggplot(data, mapping=aes(x=temp, y = count)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+weather <- ggplot(data, mapping = aes(x = weather)) +
+  geom_bar()
+
+season <- ggplot(data, mapping=aes(x=season)) +
+  geom_bar()
+
+holiday <- ggplot(data, mapping = aes(x = holiday)) +
+  geom_bar()
+
+
+(holiday + weather) / (temp + humid)
+
+sum(data$weather == 4)
+
+ggplot(data, mapping = aes(x = windspeed, y = count)) +
+  geom_point()
